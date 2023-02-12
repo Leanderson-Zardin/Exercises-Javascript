5895627156d5aa4037a27e003b403d8fb0a64e96
Leanderson Zardin <leandersonzardin1997@gmail.com>
2023-02-12 09:45:32 -0300

    organization

---
+++ first exercises/index.js
@@ -0,0 +1,21 @@
+// let person = {name: "João", age: 18, book: "Harry Potter", country: "Brasil"};
+// let x = "Um rapaz chamado " + person.name + " com " + person.age + " anos foi deportado para o " + person.country + " por gostar muito de " + person.book;
+// let y = `Um rapaz chamado ${person.name} com ${person.age} anos foi deportado para o ${person.country} por gostar muito de ${person.book}.`
+
+
+// var myAge = prompt("how old are you?", "");
+// alert(`You are ${myAge} years old.`);
+
+// let isBoss = confirm("Are you the Boss?");
+// alert(isBoss)
+
+// let str = 5 === 6;
+// alert(Boolean(str));
+
+// let x = 10;
+// let y = 5;
+// alert(String(x) + String(y));
+
+// let a = prompt("First number?", 1);
+// let b = prompt("Second number?", 2);
+// alert(+a + +b);

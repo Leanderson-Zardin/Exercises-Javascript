751ebc9d8d497aa62b051d5e65820c0089d85dd6
Leanderson Zardin <leandersonzardin1997@gmail.com>
2023-02-22 08:53:17 -0300

    Menu

---
+++ 8 - OBJECTS/Menu/menu.js
@@ -0,0 +1,17 @@
+function multiplyMenu(obj) {
+  for (let key in obj) {
+    if (typeof obj[key] == "number") {
+      obj[key] *= 2;
+    }
+  }
+}
+
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu",
+}
+
+console.log(menu)
+multiplyMenu(menu);
+console.log(menu);

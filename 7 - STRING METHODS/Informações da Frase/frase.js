d70fd0bca1c31ffbed536c237c56cf979e177be1
Leanderson Zardin <leandersonzardin1997@gmail.com>
2023-03-29 10:19:31 -0300

    Informações da Frase Atualizado

--- 7 - STRING METHODS/Informações da Frase/frase.js
+++ 7 - STRING METHODS/Informações da Frase/frase.js
@@ -1,14 +1,30 @@
-let frase = prompt("Digite uma frase: ", "");
+let frase = prompt("Digite uma Frase: ", "");
 
 let caracteres = frase.length;
-let palavras = 0;
-let lista = frase.split(" ");
+let palavras = 1;
+let espacos = 0;
+let vogal = 0;
 
-for (let i of lista) {
-  palavras++;
+for(let i of frase.toLowerCase()){
+  if(i == " ") {
+    espacos++;
+  }
+
+  if(espacos == frase.length) {
+    palavras = 0;
+  } else {
+    palavras = espacos + 1;
+  }
+
+  if(i == "a" || i == "e" || i == "i" || i == "o" || i == "u" || i == "é") {
+    vogal++
+  }
 }
 
 alert("Informações da Frase \n" +
-  "\nNúmero de caracteres: " + caracteres +
-  "\nNúmero de Palavras: " + palavras
+  "\n" + frase + "\n" +
+  "\nCaracteres: " + caracteres +
+  "\nPalavras: " + palavras +
+  "\nEspaços: " + espacos +
+  "\nVogais: " + vogal
 )

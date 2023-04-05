7d2b2badf15f4c215e6e13118dfa6880d2965a10
Leanderson Zardin <leandersonzardin1997@gmail.com>
2023-04-05 08:55:10 -0300

    Substituir Letra Atualizado

--- 7 - STRING METHODS/Substituir Letra/substituir.js
+++ 7 - STRING METHODS/Substituir Letra/substituir.js
@@ -1,8 +1,22 @@
 let str = prompt("Digite uma palavra");
 alert( upperCase(str) );
+let letra = prompt("Digite uma letra para substituir as vogais:", "");
 
 function upperCase(str) {
   if (!str) return "Você não digitou uma palavra";
 
   return (str[0].toUpperCase() + str.slice(1));
 }
+
+function vogal(str, letra) {
+  let resultado = "";
+  for(let i of str) {
+    if(i == "a" || i == "e" || i == "i" || i == "o" || i == "u") {
+      str = str.replace(i, letra);
+    }
+  }
+
+  return str;
+}
+
+alert( vogal(str, letra) );
